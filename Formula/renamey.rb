0bb0d0b0ee0d6ad79f3a1be39b0b47cb46dd9e1b
blporter <brookeporter13@gmail.com>
2026-09-07 22:44:04 -0700

    Remove unnecessary if statement

--- Formula/renamey.rb
+++ Formula/renamey.rb
@@ -1,14 +1,11 @@
 class Renamey < Formula
   desc "Use local AI models to bulk rename media files into clean Title Case"
   homepage "https://github.com/blporter/Renamey"
-  version "1.0.0"
+  url "https://github.com/blporter/Renamey/releases/download/v1.0.0/renamey-v1.0.0-darwin-arm64.zip"
+  sha256 "35da6db952c6295104a8753193438f2b321971f70c7b920f52b04a26dc555546"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/blporter/Renamey/releases/download/v1.0.0/renamey-v1.0.0-darwin-arm64.zip"
-      sha256 "sha256:35da6db952c6295104a8753193438f2b321971f70c7b920f52b04a26dc555546"
-    end
-  end
+  depends_on arch: :arm64
+  depends_on :macos
 
   def install
     libexec.install Dir["*"]
@@ -16,6 +13,6 @@ class Renamey < Formula
   end
 
   test do
-    assert_match "Usage:", shell_output("#{bin}/renamey --help 2>&1", 0)
+    assert_match "Usage:", shell_output("#{bin}/renamey --help 2>&1")
   end
 end
